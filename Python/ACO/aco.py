1d27cbee08d116447d06ee01799196c6ffeaed58
roy <royzhenlonglim@gmail.com>
2023-10-16 13:12:57 +1100

    created starting files for aco

---
+++ Python/ACO/aco.py
@@ -0,0 +1,18 @@
+class ACO:
+    distance: [[int]]
+
+    # Tweak-able Parameters
+    alpha = 0.5  # Information Heuristics Factor
+    beta = 0.8  # Expected Heuristics Factor
+
+
+    def __init__(self):
+        pass
+
+    def optimize(self):
+        while True:
+            pass
+            # Generate Solution
+            # Local Search Solution
+            # Update Pheromone Track
+
