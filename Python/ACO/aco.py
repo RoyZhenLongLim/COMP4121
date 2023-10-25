49a911cb4761f592f4d1e46505f6a4076ec409a7
roy <royzhenlonglim@gmail.com>
2023-10-25 18:57:22 +1100

    implemented basic implementation of ant colony optimization

--- Python/ACO/aco.py
+++ Python/ACO/aco.py
@@ -1,18 +1,104 @@
-class ACO:
-    distance: [[int]]
+import numpy as np
+
+
+class AntColony:
+    n_vertices: int
+    n_ants: int
+    n_best: int
+    adjacency_matrix: [[int]]
+    pheromones: [[float]]
 
     # Tweak-able Parameters
-    alpha = 0.5  # Information Heuristics Factor
-    beta = 0.8  # Expected Heuristics Factor
+    alpha = 1
+    beta = 1
+    evaporation_constant = 0.2
+    max_iterations = 5000
+    Q = 4
 
+    def __init__(self, n_vertices, n_ants, n_best):
+        self.n_vertices = n_vertices
+        self.n_ants = n_ants
+        self.n_best = n_best
+        self.adjacency_matrix = np.full((n_vertices, n_vertices), np.inf)
+        self.pheromones = np.full((n_vertices, n_vertices), 1)
 
-    def __init__(self):
-        pass
+    def add_edge(self, i, j, weight):
+        """
+        Adds an edge between vertex i and j
+        """
+        self.adjacency_matrix[i][j] = weight
+        self.adjacency_matrix[j][i] = weight
 
-    def optimize(self):
-        while True:
-            pass
-            # Generate Solution
-            # Local Search Solution
-            # Update Pheromone Track
+    def optimize(self) -> (str, float):
+        all_time_shortest_path = ("placeholder", np.inf)
+        for t in range(self.max_iterations):
+            all_paths = self.gen_all_paths()
+            # Pheromones from previous generation evaporates slightly
+            self.pheromones = self.pheromones * (1 - self.evaporation_constant)
+            # New pheromone from this iteration is spread
+            self.spread_pheromones(all_paths)
+            shortest_path = min(all_paths, key=lambda x: x[1])
+            # If new shortest path is found, update
+            if shortest_path[1] < all_time_shortest_path[1]:
+                all_time_shortest_path = shortest_path
 
+        return all_time_shortest_path
+
+    def spread_pheromones(self, all_paths: [[(int, int)]]):
+        """
+        Spread pheromone on the path traversed by the n_best ants
+        """
+        paths = sorted(all_paths, key=lambda x: x[1])[:self.n_best]
+        for path, path_len in paths:
+            for (a, b) in path:
+                self.pheromones[a][b] += self.Q / self.adjacency_matrix[a][b]
+                self.pheromones[b][a] += self.Q / self.adjacency_matrix[b][a]
+
+    def gen_all_paths(self) -> [[(int, int)]]:
+        """
+        For each ant, generate a random cycle depending on current pheromone levels and return it
+        """
+        all_paths = []
+        for i in range(self.n_ants):
+            path = self.gen_path(0)
+            all_paths.append((path, self.gen_path_dist(path)))
+        return all_paths
+
+    def gen_path(self, start: int) -> [(int, int)]:
+        """
+        Generates a Hamiltonian Cycle
+        """
+        path = []
+        visited = set()
+        visited.add(start)
+        prev = start
+        for i in range(self.n_vertices - 1):
+            move = self.pick_move(self.pheromones[prev], self.adjacency_matrix[prev], visited)
+            path.append((prev, move))
+            prev = move
+            visited.add(move)
+        path.append((prev, start))
+        return path
+
+    def gen_path_dist(self, path: [(int, int)]) -> int:
+        """
+        Returns the path length of a given path
+        """
+        path_length = 0
+        for a, b in path:
+            path_length += self.adjacency_matrix[a][b]
+        return path_length
+
+    def pick_move(self, pheromones: [float], distances: [int], visited: set[int]) -> int:
+        """
+        Given pheromones and distance, randomly select a vertex to move to that hasn't been visited before
+        """
+        prob = np.copy(pheromones)
+        prob[list(visited)] = 0
+
+        prob = prob ** self.alpha * ((1 / distances) ** self.beta)
+        prob = prob / np.sum(prob)
+
+        move = np.random.choice([i for i in range(self.n_vertices)], p=prob)
+
+        return move
--- Python/ACO/main.py
+++ Python/ACO/main.py
@@ -1,34 +1,25 @@
-from .travelingSalesman import TravelingSalesman
+from Python.ACO.aco import AntColony
 
 
 def main():
-    ants = 4
-    cities = 8
-    max_iterations = 200
-    ts = TravelingSalesman(ants, cities)
-    connections = [
-        (0, 1),
-        (0, 2),
-        (0, 3),
-        (0, 7),
-        (1, 3),
-        (1, 5),
-        (1, 7),
-        (2, 4),
-        (2, 5),
-        (2, 6),
-        (4, 3),
-        (4, 5),
-        (4, 7),
-        (6, 7),
+    n_vertices = 4
+    n_ants = 1
+    n_best = 1
+    edges = [
+        (0, 1, 1),
+        (1, 2, 1),
+        (2, 3, 1),
+        (3, 0, 1),
     ]
 
-    for city_i, city_j in connections:
-        ts.connect_cities(city_i, city_j)
+    aco = AntColony(n_vertices, n_ants, n_best)
 
-    ts.print_graph()
-    ts.print_pheromones()
-    ts.optimize(max_iterations)
+    for i, j, weights in edges:
+        aco.add_edge(i, j, weights)
+
+    print(
+        aco.optimize()
+    )
 
 
 if __name__ == "__main__":
